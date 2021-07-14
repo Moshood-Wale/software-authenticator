c770d0b955c7a2577e42c60b9d74604b917ff684
Decagon <decagon@Decagons-MacBook-Air.local>
2021-07-14 11:20:28 +0100

    First Commit

--- email_parser.py
+++ email_parser.py
@@ -1,4 +1,30 @@
 
-def email_parser(email):
-  pass
+# Regex Pattern module imported
+import re
+
+# A function that parses an email
+def email_parser(email) :
+    
+    # Both keys seperates the username and domain name respectively
+    keys = ['username', 'domain']
+    
+    # email_input validation    
+    pattern = re.compile(r"(^[a-z]+[a-z+]+[a-z0-9]+@[a-z][a-z0-9]+\.com$)")
+
+    # Ckecks characters in the mail
+    confirm_email = pattern.search(email)
+
+    if confirm_email:
+        
+        split_email = re.split("@", email)
+        
+        dictionary = {k:v for (k,v) in zip(keys, split_email)}
+        
+        return dictionary
+    
+    else:
+        
+        return None
+
+print(email_parser("jane+doe@yahoo.com"))
 
--- secure_resource/resource_deco.py
+++ secure_resource/resource_deco.py
@@ -1,5 +1,50 @@
+
 from auth import authenticate
+from datetime import datetime
+
+get_date = datetime.now()
+readable_date = get_date.strftime("%d/%m/%Y %H:%M")
 
 
 def resource_deco(email='example@email.com', password='example123'):
-  pass
+    
+    def role_checker(func) :
+        check_user = authenticate(email, password)
+        
+        def wrap_func() :
+            if check_user :
+                
+                if check_user["role"] == "admin" or check_user["role"] == "superadmin" :
+
+                    func()
+                    users_result= func()
+                    
+                    with open("access_granted.txt",'a') as f:
+                        f.write(f'\n{check_user["role"]} {check_user["first_name"]} {check_user["last_name"]} viewed company resources on {readable_date}')
+                    return users_result
+
+
+                elif check_user["role"] != "admin" or check_user["role"] != "superadmin" :
+                    
+                    user_log=("You are not allowed to view this resource")
+                    
+                    with open("access_denied.txt",'a') as f:
+                            f.write(f"\n{check_user['role']} {check_user['first_name']} {check_user['last_name']} tried to view company most valuable resources on {readable_date}")  
+                    return user_log  
+
+
+                
+            else:
+                return "Only staff can access this resource"
+
+        return wrap_func
+
+    return role_checker
+
+
+
+
+
+        
+
+        
